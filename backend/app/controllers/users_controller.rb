78b9a124ec3360b15063bc327c4ce6b8cfcfefbb
willfbren <willfbren@gmail.com>
2020-05-22 15:23:28 -0500

    user signup

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,25 +1,26 @@
+class UsersController < ApplicationController
 
+    def index
+        users = User.all
 
+        render(json: users)
+    end
 
-class UsersController < ApplicationControllers
-
-    
     def create 
-
         user = User.create({
-            first_name: params[:first_name],
-            last_name: params[:last_name],
+            first_name: params[:firstName],
+            last_name: params[:lastName],
             password_digest: params[:password],
             email: params[:email]
         })
 
-        render (json: user)
+        render(json: user)
     end
 
     def show    
         user = User.find(params[:id])
 
-        render (json: user)
+        render(json: user)
     end 
 
     def update 
@@ -30,7 +31,7 @@ class UsersController < ApplicationControllers
             password_digest: params[:password],
             email: params[:email]
         })
-        render (json: user)
+        render(json: user)
     end 
 
     def destroy 
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   
   resources :users, :shoes, :transactions
-
-   
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
