46bea67b9c14006745b30141300cab2d8cfcb5f1
faizah1992 <faizah1992@gmail.com>
2020-05-28 13:22:15 -0500

    about

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,7 @@ import  Cart  from './components/Cart'
 import { Createshoe } from './components/Createshoe'
 import ShoeDetails from './components/ShoeDetails'
 import { FlexboxGrid } from 'rsuite';
-import About from './components/About'
+import  About  from './components/About'
 
 
 function App(props) {
